e16d8c875bd710f17618de3ae74c22675b7e515c
ngtrhui <trihuy2004@gmail.com>
2026-01-07 15:21:37 +0700

    fix bug

--- src/components/level-card/level-card.js
+++ src/components/level-card/level-card.js
@@ -3,9 +3,12 @@ import Link from "next/link";
 export default function LevelCard({ level, locked, color }) {
     return (
         <Link
-            href={locked ? "#" : `/level/${level}`}
-            className={`block ${locked ? "pointer-events-none" : ""}`}
+            href={`/level/${level}`}
             aria-disabled={locked}
+            className={`block ${locked ? "pointer-events-none" : ""}`}
+            onClick={(e) => {
+                if (locked) e.preventDefault();
+            }}
         >
             <div
                 className={`border p-6 text-center rounded-xl text-white transition
